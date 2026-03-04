4f2cb2d9adcc817c29aef294c01725da54eded13
cdemzy <cdemzyy@gmail.com>
2026-03-03 23:43:30 -0400

    updated symbol to popcorn

--- BlossomMovie/ContentView.swift
+++ BlossomMovie/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "movieclapper")
+            Image(systemName: "popcorn")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Blossom Movie")
