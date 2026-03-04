cd0ea6d151bb727e591a81f5467ea8bf9f686d84
Charles Dela Cruz <119467519+cdemzy@users.noreply.github.com>
2026-03-03 23:16:06 -0400

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Charles on 2026-03-03.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlossomMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlossomMovie
+//
+//  Created by Charles on 2026-03-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
